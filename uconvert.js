f02fffcb7287e10b47f52f3a4d58a52ef667cccb
Sean Price <seanparade@gmail.com>
2017-05-18 15:09:25 -0400

    updating to current

--- uconvert.js
+++ uconvert.js
@@ -1,1 +1,15 @@
-document.body.style.border = "10px solid red";
+var dt = new Date();
+var currentTime = dt.toLocaleDateString('en-GB') + " "
+    + dt.getHours() + ":" + dt.getMinutes();
+
+browser.contextMenus.create({
+    id:"timeStamp",
+    title:"Timestamp here",
+    contexts:["all"]
+});
+
+browser.contextMenus.create({
+    id:"fieldConvert",
+    title:"USD -> CAD",
+    contexts:["all"]
+});
